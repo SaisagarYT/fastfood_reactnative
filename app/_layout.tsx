c3dc9c23ac7ff9b540e94fbe8a64dc237ca8b418
SaisagarYT <saisagarsylada@gmail.com>
2025-07-05 12:28:30 +0530

    2nd change

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Stack } from "expo-router";
+// import {useFonts} from 'expo-font';
+import { useEffect } from "react";
 import "./global.css"
 
 export default function RootLayout() {
-  return <Stack />;
+ 
+  return <Stack screenOptions={{headerShown:false}} />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        //main templates
+        red:{
+          100:'#D33B0D'
+        },
+        green:{
+          100:'#084137'
+        },
+        orange:{
+          100:'#EB920C'
+        },
+        darkOrange:{
+          100:'#DF5A0C'
+        },
+        buttons:{
+          100:'#FE8C00'
+        },
+        brown:{
+          100:'#3C2F2F'
+        },
+        lighttext:{
+          100:'#878787'
+        }
+      }
+    },
   },
   plugins: [],
 }
